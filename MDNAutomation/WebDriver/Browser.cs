0cb63cdbad7dc26b46530af91b7b5798385c5050
felhallador <mercenary.minsk@gmail.com>
2017-01-25 10:39:55 +0300

    test commit

---
+++ MDNAutomation/WebDriver/Browser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MDNAutomation.WebDriver
+{
+    class Browser
+    {
+        private static Browser _currentInstance;
+        private static Browser _firstInstance;
+        private static Browser _secondInstance;
+        private static int _counter = 1;
+        //private static readonly Logger Log = Logger.Instance;
+    }
+}
---
+++ MDNAutomation/WebDriver/BrowserFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.
+
+namespace MDNAutomation.WebDriver
+{
+    class BrowserFactory
+    {
+        private static string DownloadFolder { get; set; }
+        public static string GetDownloadFolder()
+        {
+            return DownloadFolder;
+        }
+    }
+}
